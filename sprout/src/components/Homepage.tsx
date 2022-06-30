5cd9a528b7a7d9c5630384925e52684354b007c2
Sasu Osayande <osayande.sasu@outlook.com>
2022-06-29 22:57:12 -0400

    Add header and component styling

---
+++ sprout/src/components/Homepage.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import './Homepage.scss'
+
+const Homepage: FC = () => (
+  <div className="homepage">
+    <div className="hmpg-title sun">
+      <header>Sprout</header>
+    </div>
+    <div className="hmpg-title sky">
+    </div>
+    <div className="hmpg-title water">
+    </div>
+    <div className="hmpg-title soil">
+    </div>
+  </div>
+);
+
+export default Homepage;
