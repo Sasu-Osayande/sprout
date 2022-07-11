d3e4986a7e1066116ef99d2e3ef9625b898272ef
Sasu Osayande <osayande.sasu@outlook.com>
2022-07-11 18:56:14 -0400

    Add basic form login

--- sprout/src/components/Login.tsx
+++ sprout/src/components/Login.tsx
@@ -4,6 +4,13 @@ import "./Login.scss";
 const Login: FC = () => (
   <div>
     <header>Login</header>
+    <div>
+      <form>
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   </div>
 );
 
