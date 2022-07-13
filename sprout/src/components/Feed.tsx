620dbad882a03731ded0625b2687a020bff6f31c
Sasu Osayande <osayande.sasu@outlook.com>
2022-07-13 11:34:08 -0400

    Create a Feed component

---
+++ sprout/src/components/Feed.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Feed.scss";
+
+const Feed = () => {
+  return (
+    <div>
+      <header>Sprout</header>
+    </div>
+  );
+}
+
+export default Feed;
