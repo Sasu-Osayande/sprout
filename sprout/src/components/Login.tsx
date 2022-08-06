76841fb947eeaaa605eb0d1dc3a2eebfb31dc1a3
Sasu Osayande <osayande.sasu@outlook.com>
2022-08-05 20:05:56 -0400

    Change classNames to be more specific

--- sprout/src/components/Login.tsx
+++ sprout/src/components/Login.tsx
@@ -8,18 +8,20 @@ const Login: FC = () => (
   <div>
     <Navbar />
     <header className="login-title">Login</header>
-    <div className="whole-container">
-    <div className="form-container">
-      <form className="login-form">
-        <input type="text" placeholder="username"></input>
-        <input type="password" placeholder="password"></input>
-        <div className="login-form-btn">
-        <Link to="/feed">
-        <button className="login-btn" type="submit">Login</button>
-        </Link>
-        </div>
-      </form>
-    </div>
+    <div className="login-whole-container">
+      <div className="login-form-container">
+        <form className="login-form">
+          <input className="login-input" type="text" placeholder="username"></input>
+          <input className="login-input" type="password" placeholder="password"></input>
+          <div className="login-form-btn">
+            <Link to="/feed">
+              <button className="login-btn" type="submit">
+                Login
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 );
