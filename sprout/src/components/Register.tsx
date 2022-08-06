69e899144b9c6dd5ee8411f1bfaae15906a80c94
Sasu Osayande <osayande.sasu@outlook.com>
2022-08-05 20:06:25 -0400

    Update styling to match Login component

--- sprout/src/components/Register.tsx
+++ sprout/src/components/Register.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import "./Register.scss";
 
 import Navbar from "./Navbar";
@@ -6,17 +7,25 @@ import Navbar from "./Navbar";
 const Register: FC = () => (
   <div>
     <Navbar />
-    <header>Register</header>
-    <div>
-      <form>
-      <input type="text" placeholder="first name"></input>
-      <input type="text" placeholder="last name"></input>
-        <input type="text" placeholder="username"></input>
-        <input type="text" placeholder="location"></input>
-        <input type="password" placeholder="password"></input>
-        <input type="password" placeholder="confirm password"></input>
-        <button type="submit">Register</button>
-      </form>
+    <header className="register-title">Register</header>
+    <div className="reg-whole-container">
+      <div className="reg-form-container">
+        <form className="register-form">
+          <input className="reg-input" type="text" placeholder="first name"></input>
+          <input className="reg-input" type="text" placeholder="last name"></input>
+          <input className="reg-input" type="text" placeholder="username"></input>
+          <input className="reg-input" type="text" placeholder="location"></input>
+          <input className="reg-input" type="password" placeholder="password"></input>
+          <input className="reg-input" type="password" placeholder="confirm password"></input>
+          <div className="register-form-btn">
+            <Link to="/feed">
+              <button className="register-btn" type="submit">
+                Register
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 );
