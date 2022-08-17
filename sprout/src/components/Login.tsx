963b723ece7257a590eb65baedefd99b97b513ca
Sasu Osayande <osayande.sasu@outlook.com>
2022-08-17 13:56:46 -0400

    Refactor and move theme context into its own file

--- sprout/src/components/Login.tsx
+++ sprout/src/components/Login.tsx
@@ -1,40 +1,11 @@
-import { FC } from "react";
+// import { FC } from "react";
 import { Link } from "react-router-dom";
-// import { createContext, useState } from "react";
-import {createGlobalStyle, css} from "styled-components";
 import "./Login.scss";
 
 import Navbar from "./Navbar";
 
-interface Props {
-  theme: string
-}
-
-// use the createGlobalStyles function to define colours and apply them conditionally
-export const GlobalStyles = createGlobalStyle(
-  (props: Props) => css`
-    :root {
-      --color-dark: hsl(0, 0%, 10%);
-      --color-light: hsl(0, 0%, 95%);
-    }
-
-    body {
-      background-color: ${props.theme === "light" ? "var(--color-light)" : "var(--color-dark)"};
-      color: ${props.theme === "light" ? "var(--color-dark)" : "var(--color-light)"};
-    }
-  `,
-)
-
-// export const ThemeContext = createContext(null);
-
 function Login() {
-  // const [theme, setTheme] = useState("light")
-
-  // const toggleTheme = () => {
-  //   setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  // };
   return (
-  // <ThemeContext.Provider value={{ theme, toggleTheme }}>
   <div id="light">
     <Navbar />
     <header className="login-title">Login</header>
@@ -54,7 +25,6 @@ function Login() {
       </div>
     </div>
   </div>
-  // </ThemeContext.Provider>
 );
   }
 
